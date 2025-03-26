124ada0508901286afee8798f8220ea327b176f4
AnnaSolox <anna.sol.loz@gmail.com>
2025-03-26 10:13:25 +0100

    LazyVerticalGrid responsivo según orientación

--- app/src/main/java/com/example/movies_compose/ui/screens/FavoriteScreen.kt
+++ app/src/main/java/com/example/movies_compose/ui/screens/FavoriteScreen.kt
@@ -1,12 +1,16 @@
 package com.example.movies_compose.ui.screens
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.movies_compose.R
 import com.example.movies_compose.ui.components.LoadingComponent
 import com.example.movies_compose.ui.components.MovieCard
 import com.example.movies_compose.ui.components.NoFavorites
@@ -17,6 +21,13 @@ fun FavoriteScreen(navigateToDetail: (Int) -> Unit, viewModel: MovieViewModel) {
 
     val favoriteMovies = viewModel.favorites.collectAsLazyPagingItems()
 
+    val configuration = LocalConfiguration.current
+    val isPortrait = configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT
+
+    val context = LocalContext.current
+    val columns = context.resources.getInteger(
+        if(isPortrait) R.integer.grid_column_count_v else R.integer.grid_column_count_h)
+
     if (favoriteMovies.loadState.refresh is LoadState.Loading) {
         LoadingComponent()
     }
@@ -25,7 +36,8 @@ fun FavoriteScreen(navigateToDetail: (Int) -> Unit, viewModel: MovieViewModel) {
         NoFavorites()
     }
 
-    LazyColumn(
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(columns),
         modifier = Modifier
             .padding(horizontal = 8.dp)
     ) {
--- app/src/main/java/com/example/movies_compose/ui/screens/PopularScreen.kt
+++ app/src/main/java/com/example/movies_compose/ui/screens/PopularScreen.kt
@@ -2,14 +2,18 @@ package com.example.movies_compose.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.movies_compose.R
 import com.example.movies_compose.ui.components.LoadingComponent
 import com.example.movies_compose.ui.components.MovieCard
 import com.example.movies_compose.ui.viewModels.MovieViewModel
@@ -26,7 +30,17 @@ fun PopularScreen(navigateToDetail: (Int) -> Unit, viewModel: MovieViewModel) {
         LoadingComponent()
     }
 
-    LazyColumn(
+    val configuration = LocalConfiguration.current
+    val isPortrait = configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT
+
+    val context = LocalContext.current
+    val columns = context.resources.getInteger(
+        if(isPortrait) R.integer.grid_column_count_v else R.integer.grid_column_count_h)
+
+
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(columns),
         modifier = Modifier
             .padding(horizontal = 8.dp)
             .fillMaxSize()
